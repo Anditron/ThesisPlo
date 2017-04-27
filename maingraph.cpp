3e2cfed45bd71b1753b9f2f0406aa8c7ce6fae73
Anditron <dayndy@gmail.com>
2017-04-27 15:04:39 +0200

    Revamped the code and application

--- maingraph.cpp
+++ maingraph.cpp
@@ -1,5 +1,6 @@
 #include "maingraph.h"
 #include "ui_maingraph.h"
+#include "QDebug"
 
 
 
@@ -9,6 +10,23 @@ mainGraph::mainGraph(QWidget *parent) :
 {
     ui->setupUi(this);
     initGraph();
+    nbrOfGraphs = 0;
+
+    //Set the different scatter styles
+    shapes << QCPScatterStyle::ssCross;
+    shapes << QCPScatterStyle::ssPlus;
+    shapes << QCPScatterStyle::ssCircle;
+    shapes << QCPScatterStyle::ssDisc;
+    shapes << QCPScatterStyle::ssSquare;
+    shapes << QCPScatterStyle::ssDiamond;
+    shapes << QCPScatterStyle::ssStar;
+    shapes << QCPScatterStyle::ssTriangle;
+    shapes << QCPScatterStyle::ssTriangleInverted;
+    shapes << QCPScatterStyle::ssCrossSquare;
+    shapes << QCPScatterStyle::ssPlusSquare;
+    shapes << QCPScatterStyle::ssCrossCircle;
+    shapes << QCPScatterStyle::ssPlusCircle;
+    shapes << QCPScatterStyle::ssPeace;
 }
 
 mainGraph::~mainGraph()
@@ -16,6 +34,269 @@ mainGraph::~mainGraph()
     delete ui;
 }
 
+void mainGraph::initGraph()
+{
+    ui->radioBtnAll->setChecked(true);
+    ui->graph->yAxis->ticker()->setTickCount(10);
+    ui->graph->xAxis->ticker()->setTickCount(10);
+    ui->graph->xAxis->setRangeLower(0);
+    ui->graph->yAxis->setRangeLower(0);
+    ui->graph->setInteraction(QCP::iRangeDrag, true);
+    ui->graph->setInteraction(QCP::iRangeZoom, true);
+    ui->graph->axisRect()->setRangeZoom(Qt::Horizontal);
+    ui->graph->axisRect()->setRangeZoom(Qt::Vertical);
+}
+
+/*************************************************
+ *      PLOTTING FUNCTIONS
+ */
+
+/*
+ * Reads the filepath and plots the graph (extension)
+ */
+
+void mainGraph::plotFromFile(QString filepath, QString fileName) {
+    int graphNbr = getCurrentGraphNbr();
+    int count = 0;
+    QFile inputFile(filepath);
+    QVector<double> x;
+    QVector<double> y;
+    //Initiates the graph
+    if (inputFile.open(QIODevice::ReadOnly))
+    {
+       QTextStream in(&inputFile);
+       while (!in.atEnd())
+       {
+          QString line = in.readLine();
+          QStringList xy = line.split(',');
+          if (xy[6].compare(fileName) == 0) {
+              count++;
+              //qDebug() << xy[].toDouble();
+              x.append(xy[8].toDouble());
+              y.append(xy[7].toDouble());
+          }
+       }
+       ui->graph->legend->setVisible(true);
+       ui->graph->graph(graphNbr)->setName(fileName + " reads: " + QString::number(count));
+       ui->graph->graph(graphNbr)->setData(x,y);
+       ui->graph->graph(graphNbr)->setPen(getPen());
+       ui->graph->rescaleAxes();
+       ui->graph->graph(graphNbr)->setLineStyle(QCPGraph::lsNone);
+       ui->graph->graph(graphNbr)->setScatterStyle(getScatterStyle());
+       //ui->graph->yAxis->setRange(0, 0.0010);
+       ui->graph->replot();
+       qDebug() << "Execution finished";
+    } else {
+       qDebug() << "Fail opening file";
+    }
+    inputFile.close();
+}
+
+
+/*
+ * Reads the filepath and plots the graph (not invoked)
+ */
+
+void mainGraph::plotFromFileAll(QString filepath) {
+    int graphNbr = getCurrentGraphNbr();
+    int count = 0;
+    QFile inputFile(filepath);
+    QVector<double> x;
+    QVector<double> y;
+    //Initiates the graph
+    if (inputFile.open(QIODevice::ReadOnly))
+    {
+       QTextStream in(&inputFile);
+       while (!in.atEnd())
+       {
+          QString line = in.readLine();
+          QStringList xy = line.split(',');
+          if (xy[6].compare("jobtotal") == 0) {
+              count++;
+              x.append(xy[8].toDouble());
+              y.append(xy[7].toDouble());
+          }
+       }
+
+       //Legend Setup
+       ui->graph->legend->setVisible(true);
+       QString tag = filepath.split("/")[filepath.length()];
+       ui->graph->graph(graphNbr)->setName(tag);
+       ui->graph->graph(graphNbr)->setData(x,y);
+       ui->graph->graph(graphNbr)->setPen(getPen());
+
+       //Tweak axes
+       ui->graph->rescaleAxes();
+       ui->graph->graph(graphNbr)->setLineStyle(QCPGraph::lsNone);
+       ui->graph->graph(graphNbr)->setScatterStyle(getScatterStyle());
+       //ui->graph->yAxis->setRange(0, 0.0010);
+       ui->graph->replot();
+       qDebug() << "Execution finished";
+    } else {
+       qDebug() << "Fail opening file";
+    }
+    inputFile.close();
+}
+
+
+// Plots the average readspeed per job (All)
+
+void mainGraph::plotFromFileReadSpeed(QString filepath) {
+    int graphNbr = getCurrentGraphNbr();
+
+    int jobPerSec = 0;
+    int readCount = 0;
+    QFile inputFile(filepath);
+    QVector<double> x;
+    QVector<double> y;
+
+    //Comparator: checks if timestamp has changed
+    QString comp = "";
+
+    if (inputFile.open(QIODevice::ReadOnly))
+    {
+       QTextStream in(&inputFile);
+
+       //Main read line-by-line loop
+       while (!in.atEnd())
+       {
+
+          QString line = in.readLine();
+          QStringList xy = line.split(',');
+
+          //Assumes there is a one to one correspondance between jobtotal-line and a job (ID)
+          if (xy[6].compare("jobtotal") == 0) {
+              readCount++;
+              if (xy[0].compare(comp) != 0) {
+                  x.append(readCount);
+                  y.append(jobPerSec);
+                  comp = xy[0];
+                  jobPerSec = 1;
+              } else {
+                  jobPerSec++;
+              }
+          }
+       }
+       ui->graph->legend->setVisible(true);
+       ui->graph->graph(graphNbr)->setName(graphTag(filepath));
+       ui->graph->graph(graphNbr)->setData(x,y);
+       ui->graph->graph(graphNbr)->setPen(getPen());
+       ui->graph->rescaleAxes();
+
+       ui->graph->graph(graphNbr)->setLineStyle(QCPGraph::lsNone);
+       ui->graph->graph(graphNbr)->setScatterStyle(getScatterStyle());
+
+       //ui->graph->yAxis->setRange(0, 0.0010);
+       ui->graph->replot();
+       qDebug() << "Execution finished";
+    } else {
+       qDebug() << "Fail opening file";
+    }
+    inputFile.close();
+}
+
+/*************************************************
+ *      UI EVENTS
+ */
+
+void mainGraph::on_uploadBtn_clicked()
+{
+    QString filename = QFileDialog::getOpenFileName(this,
+                                                    tr("Open plotfile"),
+                                                    QDir::homePath(),
+                                                    "All files (*.*)");
+    ui->pathLineEdit->setText(filename);
+    if (ui->radioBtnAll->isChecked()) {
+         plotFromFileReadSpeed(filename);
+    }
+
+    else {
+        QString extension = ui->nameLineEdit->text();
+        plotFromFile(filename, extension);
+    }
+}
+
+
+void mainGraph::on_radioBtnAll_toggled(bool checked)
+{
+    ui->nameLineEdit->setDisabled(checked);
+}
+
+void mainGraph::on_btnAddGraph_clicked()
+{
+    ui->graph->addGraph();
+    ui->listGraphs->addItem("Graph " + QString::number(nbrOfGraphs++));
+}
+
+void mainGraph::on_btnRmvGraph_clicked()
+{
+    getCurrentGraphNbr();
+    QList<QListWidgetItem*> items = ui->listGraphs->selectedItems();
+    foreach(QListWidgetItem * item, items)
+    {
+        ui->graph->removeGraph(getCurrentGraphNbr());
+        delete ui->listGraphs->takeItem(ui->listGraphs->row(item));
+    }
+    ui->graph->replot();
+}
+
+
+void mainGraph::on_btnSave_clicked()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,
+            tr("Save graph as PDF"), "",
+            tr("All Files (*)"));
+    if (!fileName.isEmpty()) {
+        ui->graph->savePdf(fileName);
+    }
+}
+
+void mainGraph::on_btnYzoom_clicked()
+{
+    ui->graph->axisRect()->setRangeZoom(Qt::Vertical);
+}
+
+void mainGraph::on_btnXzoom_clicked()
+{
+    ui->graph->axisRect()->setRangeZoom(Qt::Horizontal);
+}
+
+
+
+
+/***************************************************
+ *      PRIVATE HELP FUNCTIONS
+ */
+
+int mainGraph::getCurrentGraphNbr() {
+    QList<QListWidgetItem*> items = ui->listGraphs->selectedItems();
+    foreach(QListWidgetItem * item, items)
+    {
+        return item->text().split(" ")[1].toInt();
+    }
+}
+
+QCPScatterStyle::ScatterShape mainGraph::getScatterStyle() {
+    if (styleIndex == 11) {
+        styleIndex = 0;
+        return shapes.at(styleIndex);
+    }
+    return shapes.at(styleIndex++);
+}
+
+QPen mainGraph::getPen() {
+    pen.setColor(QColor(qSin(styleIndex*0.3)*100+100, qSin(styleIndex*0.6+0.7)*100+100, qSin(styleIndex*0.4+0.6)*100+100));
+    return pen;
+}
+
+QString mainGraph::graphTag(QString filepath) {
+    QStringList tags = filepath.split("/");
+    return "" + tags[tags.length()-3] + "/" + tags[tags.length()-2];
+}
+
+/*************************************************
+ *      DEBUGGING FUNCTIONS
+ */
 
 /*
  * Debugging purposes: outputLine
@@ -58,254 +339,3 @@ void mainGraph::debug_checkRunTime(QString filepath) {
     }
 }
 
-
-/*
- * Deprecated: Reads the filepath and plots the graph (All)
- */
-
-void mainGraph::plotFileAll(QString filepath) {
-    QFile inputFile(filepath);
-    QVector<double> x;
-    QVector<double> y;
-    //Initiates the graph
-    if (inputFile.open(QIODevice::ReadOnly))
-    {
-       QTextStream in(&inputFile);
-       while (!in.atEnd())
-       {
-          QString line = in.readLine();
-          QStringList xy = line.split(',');
-          if (xy[5].compare("all") == 0) {
-              QDateTime qdt = qdt.fromString(xy[0], "yyyy-MM-ddThh:mm:ssZ");
-              x.append(qdt.toTime_t());
-              y.append(xy[7].toDouble());
-          }
-       }
-       ui->graph->graph(0)->setData(x,y);
-       ui->graph->rescaleAxes();
-       ui->graph->graph(0)->setLineStyle(QCPGraph::lsNone);
-       ui->graph->graph(0)->setScatterStyle(QCPScatterStyle::ssCross);
-       ui->graph->yAxis->setRange(0, 0.05);
-       ui->graph->replot();
-       qDebug() << "Execution finished";
-    } else {
-       qDebug() << "Fail opening file";
-    }
-    inputFile.close();
-}
-
-/*
- * Deprecated: Reads the filepath and plots the graph (File). Reads original PerfData
- */
-
-void mainGraph::plotFile(QString filepath, QString fileName) {
-    int count = 0;
-    QFile inputFile(filepath);
-    QVector<double> x;
-    QVector<double> y;
-    //Initiates the graph
-    if (inputFile.open(QIODevice::ReadOnly))
-    {
-       QTextStream in(&inputFile);
-       while (!in.atEnd())
-       {
-          QString line = in.readLine();
-          qDebug() << filepath;
-
-          QStringList xy = line.split(',');
-          if (xy[6].compare(fileName) == 0) {
-              count++;
-              QDateTime qdt = qdt.fromString(xy[0], "yyyy-MM-ddThh:mm:ssZ");
-              x.append(qdt.toTime_t());
-              y.append(xy[7].toDouble());
-          }
-       }
-       ui->graph->legend->setVisible(true);
-       ui->graph->graph(0)->setName(fileName + " reads: " + QString::number(count));
-       ui->graph->graph(0)->setData(x,y);
-       ui->graph->rescaleAxes();
-       ui->graph->graph(0)->setLineStyle(QCPGraph::lsNone);
-       ui->graph->graph(0)->setScatterStyle(QCPScatterStyle::ssCross);
-       ui->graph->yAxis->setRange(0, 0.0010);
-       ui->graph->replot();
-       qDebug() << "Execution finished";
-    } else {
-       qDebug() << "Fail opening file";
-    }
-    inputFile.close();
-}
-
-/*
- * Reads the filepath and plots the graph (File)
- */
-
-void mainGraph::plotFromFile(QString filepath, QString fileName) {
-    int count = 0;
-    QFile inputFile(filepath);
-    QVector<double> x;
-    QVector<double> y;
-    //Initiates the graph
-    if (inputFile.open(QIODevice::ReadOnly))
-    {
-       QTextStream in(&inputFile);
-       while (!in.atEnd())
-       {
-          QString line = in.readLine();
-          QStringList xy = line.split(',');
-          if (xy[6].compare(fileName) == 0) {
-              count++;
-              //qDebug() << xy[].toDouble();
-              x.append(xy[8].toDouble());
-              y.append(xy[7].toDouble());
-          }
-       }
-       ui->graph->legend->setVisible(true);
-       ui->graph->graph(0)->setName(fileName + " reads: " + QString::number(count));
-       ui->graph->graph(0)->setData(x,y);
-       ui->graph->rescaleAxes();
-       ui->graph->graph(0)->setLineStyle(QCPGraph::lsNone);
-       ui->graph->graph(0)->setScatterStyle(QCPScatterStyle::ssCross);
-       //ui->graph->yAxis->setRange(0, 0.0010);
-       ui->graph->replot();
-       qDebug() << "Execution finished";
-    } else {
-       qDebug() << "Fail opening file";
-    }
-    inputFile.close();
-}
-
-
-/*
- * Reads the filepath and plots the graph (All)
- */
-
-void mainGraph::plotFromFileAll(QString filepath) {
-    int count = 0;
-    QFile inputFile(filepath);
-    QVector<double> x;
-    QVector<double> y;
-    //Initiates the graph
-    if (inputFile.open(QIODevice::ReadOnly))
-    {
-       QTextStream in(&inputFile);
-       while (!in.atEnd())
-       {
-          QString line = in.readLine();
-          QStringList xy = line.split(',');
-          if (xy[6].compare("jobtotal") == 0) {
-              count++;
-              x.append(xy[8].toDouble());
-              y.append(xy[7].toDouble());
-          }
-       }
-
-       //Legend Setup
-       ui->graph->legend->setVisible(true);
-       ui->graph->graph(0)->setName("Reads: " + QString::number(count));
-
-       ui->graph->graph(0)->setData(x,y);
-
-       //Tweak axes
-       ui->graph->rescaleAxes();
-       ui->graph->graph(0)->setLineStyle(QCPGraph::lsNone);
-       ui->graph->graph(0)->setScatterStyle(QCPScatterStyle::ssCross);
-       //ui->graph->yAxis->setRange(0, 0.0010);
-       ui->graph->replot();
-       qDebug() << "Execution finished";
-    } else {
-       qDebug() << "Fail opening file";
-    }
-    inputFile.close();
-}
-
-void mainGraph::plotFromFileReadSpeed(QString filepath) {
-    QSharedPointer<QCPAxisTickerFixed> fixedTicker(new QCPAxisTickerFixed);
-    fixedTicker->setTickStep(250);
-    ui->graph->xAxis->setTicker(fixedTicker);
-
-    int jobPerSec = 0;
-    int readCount = 0;
-    QFile inputFile(filepath);
-    QVector<double> x;
-    QVector<double> y;
-
-    //Comparator: checks if timestamp has changed
-    QString comp = "";
-
-    if (inputFile.open(QIODevice::ReadOnly))
-    {
-       QTextStream in(&inputFile);
-
-       //Main read line-by-line loop
-       while (!in.atEnd())
-       {
-
-          QString line = in.readLine();
-          QStringList xy = line.split(',');
-
-          //Assumes there is a one to one correspondance between jobtotal-line and a job (ID)
-          if (xy[6].compare("jobtotal") == 0) {
-              readCount++;
-              if (xy[0].compare(comp) != 0) {
-                  x.append(readCount);
-                  y.append(jobPerSec);
-                  comp = xy[0];
-                  jobPerSec = 1;
-              } else {
-                  jobPerSec++;
-              }
-          }
-       }
-       ui->graph->legend->setVisible(true);
-       ui->graph->graph(0)->setName("Jobs read: " + QString::number(readCount));
-       ui->graph->graph(0)->setData(x,y);
-       ui->graph->rescaleAxes();
-       ui->graph->graph(0)->setLineStyle(QCPGraph::lsNone);
-       ui->graph->graph(0)->setScatterStyle(QCPScatterStyle::ssCross);
-       //ui->graph->yAxis->setRange(0, 0.0010);
-       ui->graph->replot();
-       qDebug() << "Execution finished";
-    } else {
-       qDebug() << "Fail opening file";
-    }
-    inputFile.close();
-}
-
-
-
-void mainGraph::on_uploadBtn_clicked()
-{
-    QString filepath = QDir::homePath() + ui->pathLineEdit->text();
-    if (ui->radioBtnAll->isChecked()) {
-         plotFromFileReadSpeed(filepath);
-    } else {
-        QString filename = ui->nameLineEdit->text();
-        plotFromFile(filepath, filename);
-    }
-}
-
-void mainGraph::initGraph()
-{
-    ui->graph->addGraph();
-    ui->graph->xAxis->setRange(QDateTime::currentDateTime().toTime_t(), QDateTime::currentDateTime().toTime_t() + 3600*24*10);
-    ui->graph->yAxis->setRange(0, 0.01);
-    ui->graph->replot();
-    QSharedPointer<QCPAxisTickerDateTime> dateTicker(new QCPAxisTickerDateTime);
-    dateTicker->setDateTimeFormat("hh:mm:ss:zzz");
-    ui->graph->xAxis->setTicker(dateTicker);
-    ui->graph->setInteraction(QCP::iRangeDrag, true);
-    ui->graph->setInteraction(QCP::iRangeZoom, true);
-    ui->graph->axisRect()->setRangeZoom(Qt::Horizontal);
-    ui->radioBtnAll->setChecked(true);
-}
-
-void mainGraph::on_radioBtnAll_toggled(bool checked)
-{
-    ui->nameLineEdit->setDisabled(checked);
-}
-
-
-void mainGraph::on_btnZoomIn_clicked()
-{
-
-}
--- maingraph.h
+++ maingraph.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QDebug>
+#include "qcustomplot.h"
 
 namespace Ui {
 class mainGraph;
@@ -19,19 +20,43 @@ public:
 private slots:
     void on_uploadBtn_clicked();
     void on_radioBtnAll_toggled(bool checked);
+    void on_btnAddGraph_clicked();
+    void on_btnRmvGraph_clicked();
 
-    void on_btnZoomIn_clicked();
+    void on_btnSave_clicked();
+
+    void on_btnYzoom_clicked();
+
+    void on_btnXzoom_clicked();
 
 private:
     Ui::mainGraph *ui;
+    int nbrOfGraphs;
+    void initGraph();
+    int getCurrentGraphNbr();
+    QString graphTag(QString);
+
+    //Scatter styles
+    QVector<QCPScatterStyle::ScatterShape> shapes;
+    int styleIndex;
+    QPen pen;
+
+    QPen getPen();
+    QCPScatterStyle::ScatterShape getScatterStyle();
+
+    //Debug
     void debug_printOutFile(QString);
     void debug_checkRunTime(QString);
-    void plotFileAll(QString); //Deprecated, see plotFromFileAll
-    void plotFile(QString, QString); //Deprecated, see plotFromFile
+
+    //Plot functions
     void plotFromFileAll(QString);
     void plotFromFile(QString, QString);
     void plotFromFileReadSpeed(QString);
-    void initGraph();
+
+
+
+
+
 };
 
 #endif // MAINGRAPH_H
